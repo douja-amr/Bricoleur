354ea7ff78394c5296128737e701fdedf2ebcafe
elberdaoui <imad.elberdaoui@gmail.com>
2020-07-21 03:37:19 +0100

    database

---
+++ bricoleur/database/migrations/2020_07_21_022753_create_bricoleurs_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBricoleursTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bricoleur', function (Blueprint $table) {
+          $table->id();
+          $table->string('nom');
+          $table->string('prenom');
+          $table->string('telephone');
+          $table->string('CIN');
+          $table->string('email')->unique();
+          $table->binary('image');
+          $table->unsignedBigInteger('ville_id');
+          $table->foreign('ville_id')->references('id')->on('villes');
+          $table->unsignedBigInteger('secteur_id');
+          $table->foreign('secteur_id')->references('id')->on('secteur');
+          $table->unsignedBigInteger('user_id');
+          $table->foreign('user_id')->references('id')->on('users');
+          $table->string('approuver');
+          $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('bricoleur');
+    }
+}
