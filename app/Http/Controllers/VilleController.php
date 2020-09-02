a62b906a33b934f4e3a61fb3c3d2e4c821e8b112
elberdaoui <imad.elberdaoui@gmail.com>
2020-09-02 23:02:27 +0100

    add

---
+++ app/Http/Controllers/VilleController.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Ville;
+use Illuminate\Http\Request;
+
+class VilleController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $ville = Ville::all();
+        return view('/ville.index',['ville'=>$ville]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('ville/create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $prmtr = $request->except(['_token']);
+        $ville = new Ville();
+        $ville->nom_ville = $prmtr['nom_ville'];
+        $ville->save();
+        return redirect('/ville')->with('success', 'Add successfully');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Ville  $ville
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Ville $ville)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Ville  $ville
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $ville = Ville::find($id);
+        return view('ville.edit',['ville' => $ville]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Ville  $ville
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+      $ville = Ville::find($id);
+
+      $ville->nom_ville = $request['nom_ville'];
+      $ville->save();
+
+      return redirect('/ville');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Ville  $ville
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+      $ville = Ville::find($id);
+      $ville->delete();
+      return redirect('/ville');
+    }
+}
---
+++ app/Ville.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use App\Bricoleur;
+
+class Ville extends Model
+{
+    // protected $table = "villes";
+    //
+    // public function bricoleurs()
+    // {
+    //   return $this->hasMany(Bricoleur::class);
+    // }
+}
