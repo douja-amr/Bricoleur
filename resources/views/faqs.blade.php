45d2f3824c3048c31bd3d5f68ca261a3cc6baffe
Akram-Ettayfi <58331262+Akram-Ettayfi@users.noreply.github.com>
2020-09-04 00:09:56 +0100

    Add files via upload

---
+++ resources/views/faqs.blade.php
@@ -0,0 +1,170 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <link rel="stylesheet" href="{{asset('css/faqs.css')}}">
+    <link rel="stylesheet" href="{{asset('css/company.css')}}">
+  </head>
+  <body>
+
+    <div class="wrapper">
+        <div class="preloader">
+          <div class="loading"><span></span><span></span><span></span><span></span></div>
+        </div><!-- /.preloader -->
+      
+        
+    
+        <!-- ======================
+           FAQ
+        ========================= -->
+        <section class="faq">
+          <div class="container">
+            <div class="row">
+              <div class="col-sm-12 col-md-12 col-lg-4">
+                <aside class="sidebar mb-30">
+                  <div class="widget widget-help bg-theme">
+                    <div class="widget__content">
+                      <h5>Dedicated Customer Teams & Agile Services</h5>
+                      <p>Today Promin has over 4,000 professionals on its payroll All Over The World.</p>
+                      <a href="request-quote.html" class="btn btn__white btn__link">
+                        <span class="color-white">Get A Quote</span> <i class="icon-arrow-right"></i>
+                      </a>
+                    </div><!-- /.widget-download -->
+                  </div><!-- /.widget-help -->
+                  <div class="widget widget-download">
+                    <h5 class="widget__title">Download</h5>
+                    <div class="widget__content">
+                      <a href="#" class="btn btn__primary btn__block mb-20">
+                        <span>Company Report 2020</span>
+                        <img src="{{asset('images/icons/pdf.png')}} " alt="pdf">
+                      </a>
+                      <a href="#" class="btn btn__secondary btn__block btn__hover3">
+                        <span>Company Brochure</span>
+                        <img src="{{asset('images/icons/pdf.png')}} " alt="pdf">
+                      </a>
+                    </div><!-- /.widget-content -->
+                  </div><!-- /.widget-download -->
+                </aside><!-- /.sidebar -->
+              </div><!-- /.col-lg-4 -->
+              <div id="accordion" class="col-sm-12 col-md-12 col-lg-8">
+                <div class="accordion-item">
+                  <div class="accordion__item-header" data-toggle="collapse" data-target="#collapse1">
+                    <a class="accordion__item-title" href="#">Which Plan Is Right For Me?</a>
+                  </div><!-- /.accordion-item-header -->
+                  <div id="collapse1" class="collapse" data-parent="#accordion">
+                    <div class="accordion__item-body">
+                      <p>With any financial product that you buy, it is important that you know you are getting the best
+                        advice from a reputable company as often</p>
+                    </div><!-- /.accordion-item-body -->
+                  </div>
+                </div><!-- /.accordion-item -->
+                <div class="accordion-item">
+                  <div class="accordion__item-header" data-toggle="collapse" data-target="#collapse2">
+                    <a class="accordion__item-title" href="#">Do I have to commit to a contract?</a>
+                  </div><!-- /.accordion-item-header -->
+                  <div id="collapse2" class="collapse" data-parent="#accordion">
+                    <div class="accordion__item-body">
+                      <p>With any financial product that you buy, it is important that you know you are getting the best
+                        advice from a reputable company as often</p>
+                    </div><!-- /.accordion-item-body -->
+                  </div>
+                </div><!-- /.accordion-item -->
+                <div class="accordion-item opened">
+                  <div class="accordion__item-header" data-toggle="collapse" data-target="#collapse3">
+                    <a class="accordion__item-title" href="#">What Payment Methods Are Available?</a>
+                  </div><!-- /.accordion-item-header -->
+                  <div id="collapse3" class="collapse show" data-parent="#accordion">
+                    <div class="accordion__item-body">
+                      <p>With any financial product that you buy, it is important that you know you are getting the best
+                        advice from a reputable company as often</p>
+                    </div><!-- /.accordion-item-body -->
+                  </div>
+                </div><!-- /.accordion-item -->
+                <div class="accordion-item">
+                  <div class="accordion__item-header" data-toggle="collapse" data-target="#collapse4">
+                    <a class="accordion__item-title" href="#">What if I pick the wrong plan?</a>
+                  </div><!-- /.accordion-item-header -->
+                  <div id="collapse4" class="collapse" data-parent="#accordion">
+                    <div class="accordion__item-body">
+                      <p>With any financial product that you buy, it is important that you know you are getting the best
+                        advice from a reputable company as often</p>
+                    </div><!-- /.accordion-item-body -->
+                  </div>
+                </div><!-- /.accordion-item -->
+                <div class="accordion-item">
+                  <div class="accordion__item-header" data-toggle="collapse" data-target="#collapse5">
+                    <a class="accordion__item-title" href="#">Any contracts or commitments?</a>
+                  </div><!-- /.accordion-item-header -->
+                  <div id="collapse5" class="collapse" data-parent="#accordion">
+                    <div class="accordion__item-body">
+                      <p>With any financial product that you buy, it is important that you know you are getting the best
+                        advice from a reputable company as often</p>
+                    </div><!-- /.accordion-item-body -->
+                  </div>
+                </div><!-- /.accordion-item -->
+                <div class="accordion-item">
+                  <div class="accordion__item-header" data-toggle="collapse" data-target="#collapse6">
+                    <a class="accordion__item-title" href="#">What are my payment options?</a>
+                  </div><!-- /.accordion-item-header -->
+                  <div id="collapse6" class="collapse" data-parent="#accordion">
+                    <div class="accordion__item-body">
+                      <p>With any financial product that you buy, it is important that you know you are getting the best
+                        advice from a reputable company as often</p>
+                    </div><!-- /.accordion-item-body -->
+                  </div>
+                </div><!-- /.accordion-item -->
+                <div class="accordion-item">
+                  <div class="accordion__item-header" data-toggle="collapse" data-target="#collapse7">
+                    <a class="accordion__item-title" href="#">How does the free trial work?</a>
+                  </div><!-- /.accordion-item-header -->
+                  <div id="collapse7" class="collapse" data-parent="#accordion">
+                    <div class="accordion__item-body">
+                      <p>With any financial product that you buy, it is important that you know you are getting the best
+                        advice from a reputable company as often</p>
+                    </div><!-- /.accordion-item-body -->
+                  </div>
+                </div><!-- /.accordion-item -->
+                <div class="accordion-item">
+                  <div class="accordion__item-header" data-toggle="collapse" data-target="#collapse8">
+                    <a class="accordion__item-title" href="#">What Payment Methods Are Available?</a>
+                  </div><!-- /.accordion-item-header -->
+                  <div id="collapse8" class="collapse" data-parent="#accordion">
+                    <div class="accordion__item-body">
+                      <p>With any financial product that you buy, it is important that you know you are getting the best
+                        advice from a reputable company as often</p>
+                    </div><!-- /.accordion-item-body -->
+                  </div>
+                </div><!-- /.accordion-item -->
+              </div><!-- /.col-lg-8 -->
+            </div><!-- /.row -->
+          </div><!-- /.container -->
+        </section><!-- /.FAQ -->
+    
+    
+        <!-- ========================
+          Footer
+        ========================== -->
+      
+        <button id="scrollTopBtn"><i class="fa fa-long-arrow-up"></i></button>
+    
+      </div><!-- /.wrapper -->
+
+      <script src="assets/js/jquery-3.5.1.min.js"></script>
+      <script src="assets/js/plugins.js"></script>
+      <script src="assets/js/main.js"></script>
+      <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
+          integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
+          crossorigin="anonymous"></script>
+      <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
+          integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo"
+          crossorigin="anonymous"></script>
+      <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"
+          integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI"
+          crossorigin="anonymous"></script>
+  </body>
+</html>
